10bbaf4fd732d58d7b69f8690f2014b71e9007fe
Roxane <roxane.alibert@gmail.com>
2016-11-01 14:24:53 +0100

    Tout OK

--- MovieProject/app/src/main/java/com/rox/app/movieproject/Constant.java
+++ MovieProject/app/src/main/java/com/rox/app/movieproject/Constant.java
@@ -1,5 +1,7 @@
 package com.rox.app.movieproject;
 
+import com.rox.app.movieproject.Data.MovieContract;
+
 /**
  * Created by admuin on 30/10/2016.
  */
@@ -9,4 +11,13 @@ public class Constant {
     public static String URL_MOVIE = "http://api.themoviedb.org/3/movie/";
     public static String URL_YOUTUBE = "http://www.youtube.com/watch?v=";
     public static String URL_POSTERS = "http://image.tmdb.org/t/p/w185";
+    public static String[] MOVIE_COL = {
+            MovieContract.MovieEntry._ID,
+            MovieContract.MovieEntry.COLUMN_ID_MOVIE_DB,
+            MovieContract.MovieEntry.COLUMN_TITLE,
+            MovieContract.MovieEntry.COLUMN_SYNOPSIS,
+            MovieContract.MovieEntry.COLUMN_RELEASE_DATE,
+            MovieContract.MovieEntry.COLUMN_POSTER,
+            MovieContract.MovieEntry.COLUMN_RATE
+    };
 }
